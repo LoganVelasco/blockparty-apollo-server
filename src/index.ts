a4449af5cd8ce58d51d0050bcbb72588ff939c80
LoganVelasco <loganvelasco870@gmail.com>
2023-03-13 19:12:57 -0500

    Updates Introspection

--- src/index.ts
+++ src/index.ts
@@ -1,36 +1,6 @@
 import { ApolloServer } from '@apollo/server';
 import { startStandaloneServer } from '@apollo/server/standalone';
 
-// A schema is a collection of type definitions (hence "typeDefs")
-// that together define the "shape" of queries that are executed against
-// your data.
-const typeDefs = `#graphql
-  # Comments in GraphQL strings (such as this one) start with the hash (#) symbol.
-
-  # This "Book" type defines the queryable fields for every book in our data source.
-
-  type Hint {
-    id: ID!
-    text: String
-  }
-
-  type Color {
-    id: ID!
-    colorId: Int
-    hints: [Hint]
-  }
-
-  # The "Query" type is special: it lists all of the available queries that
-  # clients can execute, along with the return type for each. In this
-  # case, the "books" query returns an array of zero or more Books (defined above).
-
-  type Query {
-    hints: [Hint]
-    colors: [Color]
-  }
-`;
-
-
 const hints = [
   {
     id: 1,
@@ -39,6 +9,14 @@ const hints = [
   {
      id: 2,
      text: 'Disable hints in settings',
+  },
+  {
+     id: 3,
+     text: 'New Hint',
+  },
+  {
+     id: 4,
+     text: 'Avoid the Enemy Block',
   },
 ];
 
@@ -54,26 +32,40 @@ const colors = [
   },
 ];
 
-// Resolvers define how to fetch the types defined in your schema.
-// This resolver retrieves books from the "books" array above.
-const resolvers = {
-  Query: {
-    hints: () => hints,
-    colors: () => colors,
-  },
-};
+ const typeDefs = `#graphql
+   type Hint {
+     id: ID!
+     text: String
+   }
 
-// The ApolloServer constructor requires two parameters: your schema
-// definition and your set of resolvers.
+   type Color {
+     id: ID!
+     colorId: Int
+     hints: [Hint]
+   }
+
+
+   type Query {
+     hints: [Hint]
+     colors: [Color]
+   }
+ `;
+
+ const resolvers = {
+     Query: {
+       hints: () => hints,
+       colors: () => colors,
+     },
+   };
+
+
 const server = new ApolloServer({
   typeDefs,
   resolvers,
+  introspection : true
 });
 
-// Passing an ApolloServer instance to the `startStandaloneServer` function:
-//  1. creates an Express app
-//  2. installs your ApolloServer instance as middleware
-//  3. prepares your app to handle incoming requests
+
 const port = Number.parseInt(process.env.PORT) || 4000;
 
 const { url } = await startStandaloneServer(server, { listen: { port } });
